62cc57f04e4d8681141e503f13711f843198f096
alishayb <alishayumnab@gmail.com>
2024-03-04 09:41:03 +0700

    rename main index

--- server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const express = require("express");
-
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-app.listen(port, () => console.log(`Listening on port ${port}`));
